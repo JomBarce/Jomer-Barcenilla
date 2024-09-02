f5c55869fb86ea8f14904889204a3888af15f111
Jomer Barcenilla <jomerbarcenilla@gmail.com>
2024-09-03 02:00:44 +0800

    Added Initial Codes

---
+++ Portfolio/JS/scripts.js
@@ -0,0 +1,32 @@
+function goToPage(url) {
+    window.location = url;
+}
+
+function goToSite(url) {
+  window.open(url, "_blank");
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  x[slideIndex-1].setAttribute("style", "display:block;");  
+}
